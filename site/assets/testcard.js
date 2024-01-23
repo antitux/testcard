6ca362e620f7ff6c1023e2e77056f770e2bbac4c
antitux <antitux@gmail.com>
2024-01-23 23:25:26 +0000

    Adding spinner

--- site/assets/testcard.js
+++ site/assets/testcard.js
@@ -1,4 +1,3 @@
-
 (function(window, document) {
   var decode = function(t) {
     var ta = document.createElement("textarea");
@@ -39,21 +38,30 @@
 
   if (queryString().banner) {
     if (queryString().cursor) {
-      document.getElementById('banner').innerHTML = decodeURI(queryString().banner) + '<span id="cursor">' + decodeURI(queryString().cursor) + '</span>';
-      var cursor = true;
+      document.getElementById('banner').innerHTML = decodeURI(queryString().banner) + "&nbsp" + '<span id="cursor">X</span>';
+      var cursorStatus = 0;
       if (queryString().speed) {
         var speed = decodeURI(queryString().speed);
       } else {
         var speed = 500;
       }
-//      var speed = 500;        
       setInterval(() => {
-        if(cursor) {
-          document.getElementById('cursor').style.opacity = 0;
-          cursor = false;
-        } else {
+        if(cursorStatus == 0) {
           document.getElementById('cursor').style.opacity = 1;
-          cursor = true;
+          document.getElementById('cursor').innerHTML = "|"; //"&nbsp&nbsp&nbsp";
+          cursorStatus = 1;
+        } else if (cursorStatus == 1) {
+          document.getElementById('cursor').style.opacity = 1;
+          document.getElementById('cursor').innerHTML = "/"; //decodeURI(queryString().cursor) + "&nbsp&nbsp";
+          cursorStatus = 2;
+        } else if (cursorStatus == 2) {
+          document.getElementById('cursor').style.opacity = 1;
+          document.getElementById('cursor').innerHTML = "-"; //decodeURI(queryString().cursor) + decodeURI(queryString().cursor) + "&nbsp";
+          cursorStatus = 3;
+        } else if (cursorStatus == 3) {
+          document.getElementById('cursor').style.opacity = 1;
+          document.getElementById('cursor').innerHTML = "\\"; //decodeURI(queryString().cursor) + decodeURI(queryString().cursor) + decodeURI(queryString().cursor);
+          cursorStatus = 0;
         }
       }, speed);
     } else {
